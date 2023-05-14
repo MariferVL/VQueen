4dda7782dfc43e96d163500e18587915cad2142b
Marifer Villalobos <mfvl.1186@gmail.com>
2023-05-14 17:10:04 -0400

    add an employee editor component

---
+++ src/app/edit-employee/edit-employee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-employee',
+  templateUrl: './edit-employee.component.html',
+  styleUrls: ['./edit-employee.component.css']
+})
+export class EditEmployeeComponent {
+
+}
