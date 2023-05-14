863e3210d94219dfc8bf404a05c08e4fc0041026
Marifer Villalobos <mfvl.1186@gmail.com>
2023-05-13 23:20:46 -0400

    add order-received route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { DetailedOrderComponent } from './detailed-order/detailed-order.componen
 import { KitchenComponent } from './kitchen/kitchen.component';
 import { ReadyComponent } from './ready/ready.component';
 import { LocationsComponent } from './locations/locations.component';
+import { OrderReceivedComponent } from './order-received/order-received.component';
 
 const routes: Routes = [
   { path: '', redirectTo:'/home', pathMatch: 'full'},
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: 'menu', component: MenuComponent},
   { path: 'menu/:id', component: DetailedMenuComponent},
   { path: 'order', component: DetailedOrderComponent},
+  { path: 'order-received', component: OrderReceivedComponent},
   { path: 'kitchen', component: KitchenComponent},
   { path: 'ready', component: ReadyComponent},
   { path: 'locations', component: LocationsComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,12 @@ import { KitchenComponent } from './kitchen/kitchen.component';
 import { ReadyComponent } from './ready/ready.component';
 import { LocationsComponent } from './locations/locations.component';
 import { ContactComponent } from './contact/contact.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { UsernameModalComponent } from './username-modal/username-modal.component';
+import { OrderModalComponent } from './order-modal/order-modal.component';
+import { OrderReceivedComponent } from './order-received/order-received.component';
+
 
 @NgModule({
   declarations: [
@@ -22,10 +29,16 @@ import { ContactComponent } from './contact/contact.component';
     ReadyComponent,
     LocationsComponent,
     ContactComponent,
+    UsernameModalComponent,
+    OrderModalComponent,
+    OrderReceivedComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
