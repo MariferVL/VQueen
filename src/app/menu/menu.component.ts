ed465f966c650120930fd775e12190b440f5f038
Marifer Villalobos <mfvl.1186@gmail.com>
2023-05-13 21:51:44 -0400

    add a menu component

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Menu } from '../types';
+import { fakeMenu } from '../fake-data';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+
+export class MenuComponent implements OnInit {
+  menus: Menu[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.menus = fakeMenu;
+  }
 
 }
