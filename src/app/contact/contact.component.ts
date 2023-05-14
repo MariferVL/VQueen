4b31a8f470aed523a688d75d64bfd1a3313c5335
Marifer Villalobos <mfvl.1186@gmail.com>
2023-05-13 21:50:59 -0400

    add a contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Menu } from '../types';
+import { fakeMenu } from '../fake-data';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  email: string = '';
+  message: string = '';
+  menu: Menu | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.menu = fakeMenu.find(menu => menu.id === id);
+    this.message = `Hi, I want to order all the included in ${this.menu?.id.toLowerCase}`;
+  }
+
+  sendMessage(): void {
+    alert('🍽️ **Order Received! Fresh Ingredients at Work!** 🎉\nExciting news! Your order is in motion! 🌱 \nOur team is busy sourcing the freshest ingredients, crafting a culinary masterpiece just for you. \nGet ready for an unforgettable dining experience! \nBon appétit! 😊🍴');
+    this.router.navigateByUrl('/menu');
+   //TODO: Show this message while the food is being cooked.
+    // alert('🍽️ **Exciting News! Your Food Order is Being Prepared!** 🎉 \n Your order is in safe hands! Our skilled team is meticulously selecting the freshest ingredients to craft your culinary masterpiece. 🌱🥦🍅 \nBehind the scenes, our talented chefs are passionately curating a symphony of flavors, ensuring that every bite exceeds your expectations. 🍽️✨\nGet ready to embark on a sensational gastronomic journey, where taste and quality unite to create an unforgettable dining experience. Bon appétit! 😊🍴')
+  }
 
 }
