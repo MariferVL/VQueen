0eebd12e4b9d9f5b7d156fa9f30c6dd905bd5ccd
Marifer Villalobos <mfvl.1186@gmail.com>
2023-05-14 17:10:53 -0400

    add a manager landing component

---
+++ src/app/manager/manager.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ManagerComponent } from './manager.component';
+
+describe('ManagerComponent', () => {
+  let component: ManagerComponent;
+  let fixture: ComponentFixture<ManagerComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ManagerComponent]
+    });
+    fixture = TestBed.createComponent(ManagerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/manager/manager.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager',
+  templateUrl: './manager.component.html',
+  styleUrls: ['./manager.component.css']
+})
+export class ManagerComponent {
+
+}
