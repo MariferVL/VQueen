478fefdc3ee4b745080e5d0caee255acf0a4ed6e
Marifer Villalobos <mfvl.1186@gmail.com>
2023-05-13 21:50:47 -0400

    add a detailed menu component

--- src/app/detailed-menu/detailed-menu.component.ts
+++ src/app/detailed-menu/detailed-menu.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Menu } from '../types';
+import { fakeMenu } from '../fake-data';
 
 @Component({
   selector: 'app-detailed-menu',
   templateUrl: './detailed-menu.component.html',
   styleUrls: ['./detailed-menu.component.css']
 })
-export class DetailedMenuComponent {
+export class DetailedMenuComponent implements OnInit {
+  menu: Menu | undefined; // Declare menu as a single Menu object, not an array
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.menu = fakeMenu.find(menu => menu.id === id);
+  }
 }
