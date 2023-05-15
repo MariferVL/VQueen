ad2659323a22d1ed0e3dc05c9012bdcd3fa86cbc
Marifer Villalobos <mfvl.1186@gmail.com>
2023-05-14 23:08:41 -0400

    add current data to edit form

--- src/app/edit-menu/edit-menu.component.ts
+++ src/app/edit-menu/edit-menu.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Menu } from '../types';
+import { fakeMenu } from '../fake-data';
 
 @Component({
   selector: 'app-edit-menu',
@@ -8,18 +10,30 @@ import { Router } from '@angular/router';
   styleUrls: ['./edit-menu.component.css']
 })
 export class EditMenuComponent {
+  menu: Menu = {
+    id: '',
+    name: '',
+    description: '',
+    ingredients: [],
+    allergens: [],
+    price: 0
+  };
+  
   constructor(
     private titleService: Title,
+    private route: ActivatedRoute,
     private router: Router,
     ) {}
 
   ngOnInit() {
     this.titleService.setTitle('VQAdmin - Edit Menu');
-  }
+    const id = this.route.snapshot.paramMap.get('id');
+    this.menu = fakeMenu.find(menu => menu.id === id) as Menu;
+    }
 
   onSubmit(): void {
     console.log('Saving changes');
-    this.router.navigateByUrl('/admin/menu')
+    this.router.navigateByUrl('/admin/menu');
   }
 
 }
--- src/app/menu-form/menu-form.component.ts
+++ src/app/menu-form/menu-form.component.ts
@@ -9,6 +9,14 @@ import { Menu } from '../types';
 })
 export class MenuFormComponent implements OnInit {
   @Input() buttonText: any;
+  @Input() currentName = '';
+  @Input() currentDescription = '';
+  @Input() currentIngredients: string[] = [];
+  @Input() currentAllergens: string[] = [];
+  @Input() currentIngredientsStr = this.currentIngredients.join(', ');
+  @Input() currentAllergensStr = this.currentAllergens.join(', ');
+  @Input() currentPrice = 0;
+
   id: string = '';
   name: string = '';
   description: string = '';
@@ -20,9 +28,14 @@ export class MenuFormComponent implements OnInit {
 
   @Output() onSubmit = new EventEmitter<Menu>();
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.name = this.currentName;
+    this.description = this.currentDescription;
+    this.ingredientsString = this.currentIngredientsStr;
+    this.allergensString =this.currentAllergensStr;
+    this.price = this.currentPrice;
   }
 
   onClick(): void {
@@ -33,13 +46,12 @@ export class MenuFormComponent implements OnInit {
       ingredients: this.ingredientsString.split(','),
       allergens: this.allergensString.split(','),
       price: Number(this.price),
-    }
-
-  )}
+    });
+  }
 
   /**
    * Generate unique id based on timestamp
-   * @returns string 
+   * @returns string
    */
   private generateUniqueId(): string {
     const timestamp = Date.now().toString(36);
