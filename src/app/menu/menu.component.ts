495fb0152d108bb0ba00fba1347ce6b989067497
Marifer Villalobos <mfvl.1186@gmail.com>
2023-05-18 13:06:30 -0400

    set menu ts to the db service

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,39 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { Menu } from '../types';
+import { fakeMenu } from '../fake-data';
 import { Title } from '@angular/platform-browser';
-import { MenuService } from '../services/menu.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
+
 export class MenuComponent implements OnInit {
-  menus: any[] = [];
+  menus: Menu[] = [];
 
   constructor(
-    private titleService: Title,
-    private menuService: MenuService
-  ) { }
+    private titleService: Title
+    ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.titleService.setTitle('VQ - Menu');
-    this.getProducts();
-  }
-
-  getProducts(): void {
-    //FIXME: cómo imprimo todas las páginas. Cómo se cuántas son?
-    const page = 1; 
-    const limit = 30; 
-
-    this.menuService.getProducts(page, limit).subscribe({
-      next: (response: any) => {
-        this.menus = response;
-        console.log('this.menus: ', this.menus);
-        
-      },
-      error: (error: any) => {
-        console.error(error);
-      }
-    });
+    this.menus = fakeMenu;
   }
 }
+// import { Component, OnInit } from '@angular/core';
+// import { Title } from '@angular/platform-browser';
+// import { MenuService } from '../services/menu.service';
+
+// @Component({
+//   selector: 'app-menu',
+//   templateUrl: './menu.component.html',
+//   styleUrls: ['./menu.component.css']
+// })
+// export class MenuComponent implements OnInit {
+//   menus: any[] = [];
+
+//   constructor(
+//     private titleService: Title,
+//     private menuService: MenuService
+//   ) { }
+
+//   ngOnInit(): void {
+//     this.titleService.setTitle('VQ - Menu');
+//     this.getProducts();
+//   }
+
+//   getProducts(): void {
+//     //FIXME: cómo imprimo todas las páginas. Cómo se cuántas son?
+//     const page = 1; 
+//     const limit = 30; 
+
+//     this.menuService.getProducts(page, limit).subscribe({
+//       next: (response: any) => {
+//         this.menus = response;
+//         console.log('this.menus: ', this.menus);
+        
+//       },
+//       error: (error: any) => {
+//         console.error(error);
+//       }
+//     });
+//   }
+// }
