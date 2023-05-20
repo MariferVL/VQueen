a84e2d943523620bc1942d75ee2bbcb476d5a0f9
Marifer Villalobos <mfvl.1186@gmail.com>
2023-05-20 14:40:13 -0400

    cerate an admin.service file

--- src/app/admin.service.ts
+++ src/app/admin.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Menu } from './types';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getMenus(): Observable<Menu[]> {
+    return this.http.get<Menu[]>('/api/menu');
+  }
+
+  getMenusById(id: string): Observable<Menu> {
+    return this.http.get<Menu>(`/api/menu/${id}`)
+  }
+
+  addSaleToMenu(id: string): Observable<Menu> {
+    return this.http.post<Menu>(
+      `/api/menu/${id}/sale`,
+      {},
+      httpOptions
+      );
+  }
+
+
 }
